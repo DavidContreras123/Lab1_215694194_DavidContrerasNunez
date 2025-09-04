012d76db99e38a33f9f67e1ceb92423f2e2f7d5f
DavidContreras <david.contreras.n@usach.cl>
2025-09-04 18:22:27 -0400

    first commit

---
+++ TDA_usuario_215694194_ContrerasNúñez.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(provide crear-usuario usuario-suspendido? obtener-deuda)
+
+;----- CONSTRUCTOR -----
+; Descripción: Constructor TDA usuario
+; Dominio: id (int) X nombre(string)
+; Recorrido: usuario
+; Recursión: No aplica
+(define (crear-usuario id nombre)
+  (list id nombre 0 "sin libros" "activo"))
+
+;----- PERTENENCIA -----
+; Descripción: Comprueba si el usuario está suspendido
+; Dominio: usuario
+; Recorrido: #t o #f (bool)
+; Recursión: No aplica
+(define(usuario-suspendido? usuario)
+  (string=? (list-ref usuario 4) "suspendido"))
+     
+;----- SELECTOR -----
+; Descripción: Obtiene la deuda del usuario
+; Dominio: usuario
+; Recorrido: int (deuda)
+; Recursión: No aplica
+(define(obtener-deuda usuario)
+  (list-ref usuario 2))
